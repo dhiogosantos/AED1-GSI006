e6948d6ba190ad4c6ffedb2a898bf0d3c0d57050
Dhiogo Santos <dhiogo.santos70@gmail.com>
2022-06-05 17:35:55 -0300

    Resolução LAB

--- lista2_aed1/ex09.c
+++ lista2_aed1/ex09.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void calculo(int *j, int *k, int CONSTANTE){
-    int x1, y1;
-    x1 = *j * CONSTANTE;      
-    y1 = *k * CONSTANTE;
-    printf("Resultado: (%d,%d) * %d = (%d,%d)\n", *j, *k, CONSTANTE, x1, y1);
+void calculo(int *x, int *y, int CONSTANTE){
+    int x1 = *x;
+    int y1 = *y;
+    *x = (*x) * CONSTANTE;
+    *y = (*y) * CONSTANTE;
+    printf("\nResultado: (%d,%d) * %d = (%d,%d)\n\n", x1, y1, CONSTANTE, *x, *y);
 }
 
 int main(){
@@ -13,13 +14,13 @@ int main(){
     const int CONSTANTE;
     int x, y;
 
-    printf("Digite o ponto -> x,y: ");
+    printf("\nDigite o ponto -> x,y: ");
     scanf("%d,%d", &x, &y);
 
     printf("Digite a constante: ");
     scanf("%d", &CONSTANTE);
 
-    calculo(&x, &y, CONSTANTE);    
+    calculo(&x, &y, CONSTANTE);
     
     return 0;
 }
