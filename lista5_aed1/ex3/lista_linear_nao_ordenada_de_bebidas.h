22c383eacec973e54794e2102d9923cf8e41ac0a
Dhiogo Santos <dhiogo.santos70@gmail.com>
2022-06-23 12:02:58 -0300

    Resolução Lista 5

--- lista5_aed1/ex3/lista_linear_nao_ordenada_de_bebidas.h
+++ lista5_aed1/ex3/lista_linear_nao_ordenada_de_bebidas.h
@@ -2,11 +2,18 @@
 
 TAD lista_linear_nao_ordenada_de_bebidas
 
-    - Dados: números inteiros
+    - Dados: struct Bebida (nome, volume, valor)
 
-    - Lista de operações: insere_registro, apaga_ultimo_registro, imprime_tabela
+    - Lista de operações: insere_registro, apaga_ultimo_registro, imprime_tabela, lista_vazia, lista_cheia, criar_lista
 
 Operações:
+
+    criar_lista:
+        Entrada: nenhuma
+        Pré-condição: nenhuma
+        Processo: cria uma lista e deixa ela no estado de vazia
+        Saída: endereço da lista criada
+        Pós-condição: Nenhuma
 
     insere_registro:
         Entrada: nenhuma
@@ -16,18 +23,33 @@ Operações:
         Pós-condição: Nenhuma    
 
     apaga_ultimo_registro:
-        Entrada: endereço de uma lista e o elemento (nro inteiro) a ser inserido
-        Pré-condição: lista ser válida e não estar cheia
-        Processo: inserir o elemento no final da lista 
+        Entrada: endereço de uma lista e o elemento (nome) a ser apagado
+        Pré-condição: lista ser válida e não estar vazia
+        Processo: excluir o elemento da lista 
         Saída: 0 (sucesso - elemento inserido) ou -1 (falha - elemento não inserido) 
-        Pós-condição: a lista de entrada com um número a mais
+        Pós-condição: a lista de entrada com um elemento a menos
 
     imprime_tabela:
-        Entrada: endereço de uma lista e o elemento (nro inteiro) a ser removido
+        Entrada: endereço de uma lista
         Pré-condição: lista ser válida e não estar vazia
-        Processo: percorrer a lista até encontrar o elemento desejado ou chegar ao chegar ao seu final. Se o elemento existe, remova-o da lista
+        Processo: percorrer a lista e imprimir cada posição
         Saída: 0 (sucesso - elemento removido) ou -1 (falha - elemento não removido)
-        Pós-condição: lista de entrada com um elemento a menos    
+        Pós-condição: nenhuma
+
+    lista_vazia:
+        Entrada: entrada de uma lista
+        Pré-condição: lista ser válida
+        Processo: verifica se a lista está na condição de vazia
+        Saída: 0 (sucesso - lista está vazia) ou -1 (falha - lista não está vazia)
+        Pós-condição: nenhuma
+
+    lista_cheia:
+        Entrada: endereço de uma lista
+        Pré-condição: lista ser válida
+        Processo: verifica se a lista está na condição de cheia
+        Saída: 0 (sucesso - lista está cheia) ou -1 (falha - lista não está cheia)
+        Pós-condição: nenhuma
+     
 
 */
 
